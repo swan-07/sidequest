03ef41d4505220b5cbd5f3cd4d0698dddc2b4926
julian-kbjkjbdkfjb <kickoff.faiths.0l@icloud.com>
2024-06-02 14:26:06 -0400

    revise

--- extension/popup/script.js
+++ extension/popup/script.js
@@ -47,32 +47,6 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-async function getPoints() {
-  const result = await chrome.storage.local.get(["points"]);
-  return result?.points || 0;
-}
-
-async function setPoints(points) {
-  await chrome.storage.local.set({ points });
-}
-
-async function awardPoints(awardedPoints = 1) {
-  await setPoints((await getPoints()) + awardedPoints);
-}
-
-async function getQuestCount() {
-  const result = await chrome.storage.local.get(["questCount"]);
-  return result?.questCount || 0;
-}
-
-async function registerQuest() {
-  const result = await chrome.storage.local.get(["questCount"]);
-
-  await chrome.storage.local.set({
-    questCount: (result?.questCount || 0) + 1
-  });
-}
-
 async function loadCollection() {
   const result = await chrome.storage.local.get(["collection"]);
   return result?.collection || [];
@@ -87,7 +61,6 @@ async function addToCollection(imageData) {
   }
   return collection;
 }
-
 
 function updatePointsDisplay(points) {
   const statPointsEl = document.querySelector(".stat-points");
