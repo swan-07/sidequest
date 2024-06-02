956fd00a049d9361c493430bcf6ebe0c41fbc334
0wlish <tortugavpn@gmail.com>
2024-06-02 14:53:54 -0400

    added kindness to json, added more js to kindness

--- extension/games/kindness/script.js
+++ extension/games/kindness/script.js
@@ -4,6 +4,10 @@ function hasInput() {
     document.querySelector("svg").style.display = "block";
     document.querySelector("button").style.display = "none";
     document.querySelector("input").style.display = "none";
+    (async () => {
+        await chrome.runtime.sendMessage({ type: "done" });
+        window.close();
+    })();
 }
 
 document.querySelector(".button").addEventListener("click", () => hasInput());
