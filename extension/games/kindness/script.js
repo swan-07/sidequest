070399abe5e58a6372ff1773f0666d124f8b3351
0wlish <tortugavpn@gmail.com>
2024-06-02 14:31:27 -0400

    finishing touches on "be kind" game

--- extension/games/kindness/script.js
+++ extension/games/kindness/script.js
@@ -1,6 +1,9 @@
-function getInput() {
+function hasInput() {
     let input = document.getElementById("userInput").value;
     document.getElementById("text").innerHTML = input;
+    document.querySelector("svg").style.display = "block";
+    document.querySelector("button").style.display = "none";
+    document.querySelector("input").style.display = "none";
 }
 
-document.querySelector(".button").addEventListener("click", () => getInput());
+document.querySelector(".button").addEventListener("click", () => hasInput());
