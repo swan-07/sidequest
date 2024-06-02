7128063816f7892da4ce031cce91a251782e1f37
Oliver Beresford <118030320+OliverSBeresford@users.noreply.github.com>
2024-06-02 14:59:40 -0400

    Fixed captcha game. Added new files for captcha game. 1 parent 1e3b3b commit 1e3b3b7e

---
+++ extension/games/captcha/script.js
@@ -0,0 +1,20 @@
+setTimeout(() => {
+    chrome.runtime.sendMessage({ type: "done" });
+    const dialog = document.querySelector("dialog");
+    const p = dialog.querySelector("p")
+
+    p.textContent = "Nice job! You solved the captcha!";
+
+    confetti({
+        particleCount: 10e5,
+        spread: 180,
+        origin: { y: 0.8 },
+    });
+    
+    dialog.showModal();
+
+    const button = dialog.querySelector("button");
+    button.addEventListener("click", function() {
+        window.close();
+    })
+}, 2 * 1000);
