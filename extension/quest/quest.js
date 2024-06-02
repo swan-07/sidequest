5f45eef72bdd6799117f0b22290559e640952702
stephanie <stephaniewan07@gmail.com>
2024-06-02 10:53:20 -0400

    aaa

--- extension/quest/quest.js
+++ extension/quest/quest.js
@@ -136,32 +136,65 @@ function showStars(randomQuest){
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+  var form = document.getElementById('form');
+    number = document.getElementById('number');
+  var variable;
+  form.onsubmit = function() {
+    variable = number.value;
+    updateFrequency();
+  };
+
+  function getFrequency() {
+    chrome.storage.local.get(['frequency'], (result) => {
+        const frequency = result.frequency || 5;
+        frequencyDisplay.textContent = 'Frequency: ' + frequency + ' minutes';
+
+    });
+}
+
+function saveFrequency(frequency) {
+    chrome.storage.local.set({ frequency }, () => {
+    });
+}
+
+function updateFrequency() {
+  chrome.storage.local.get(['frequency'], (result) => {
+      let frequency = result.frequency || 5;
+      frequency = variable; 
+      saveFrequency(frequency); 
+  });
+}
+
+  getFrequency()
   questRoll();
 });
 
 document.addEventListener('DOMContentLoaded', () => {
   const pointsDisplay = document.getElementById('pointsDisplay');
 
-  function getPoints() {
-      chrome.storage.local.get(['points'], (result) => {
-          const points = result.points || 0;
-          pointsDisplay.textContent = 'Points: ' + points;
-
-      });
-  }
-
-  function savePoints(points) {
-      chrome.storage.local.set({ points }, () => {
-      });
-  }
-
-  function updatePoints() {
-    chrome.storage.local.get(['points'], (result) => {
-        let points = result.points || 0;
-        points += 1; 
-        savePoints(points); 
-    });
-}
   getPoints();
   updatePoints()
 });
+
+function getPoints() {
+  chrome.storage.local.get(['points'], (result) => {
+      const points = result.points || 0;
+      pointsDisplay.textContent = 'Points: ' + points;
+
+  });
+}
+
+function savePoints(points) {
+  chrome.storage.local.set({ points }, () => {
+  });
+}
+
+function updatePoints() {
+chrome.storage.local.get(['points'], (result) => {
+    let points = result.points || 0;
+    points += 1; 
+    savePoints(points); 
+});
+}
+
+
